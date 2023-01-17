b3dd6341212ade89ca95f565319227a8658a4612
Bebob369 <60324042+Bebob369@users.noreply.github.com>
2023-01-17 16:02:57 +0400

    Update Main.java

--- task3/src/main/java/org/example/Main.java
+++ task3/src/main/java/org/example/Main.java
@@ -11,33 +11,33 @@ public class Main {
     static JsonParser parser = new JsonParser();
 
     public static void main(String[] args) {
-       //  Прочитали файлы
+       
         String testsJsonLine = readFile(args[0]);
       String valuesJsonLine = readFile(args[1]);
         System.out.println(args[0] +" " + args[1]);
-        // Работаем с Tests
+     
         JsonElement jElementTests = parser.parse(testsJsonLine);
         JsonArray childArrayTests = jElementTests
                 .getAsJsonObject()
                 .getAsJsonArray("tests"); // получить объект Place
 
-        // Работаем с Values
+      
         JsonElement jElementValues = parser.parse(valuesJsonLine);
         JsonArray childArrayValues = jElementValues
                 .getAsJsonObject()
-                .getAsJsonArray("values"); // получить объект Place
+                .getAsJsonArray("values"); 
         for (int i = 0; i < childArrayValues.size(); i++) {
-            // передаем каждый id из Value в Tests для дальнейшего сравнения и замены
+          
             JsonElement idFromValues = childArrayValues.get(i).getAsJsonObject().get("id");
             String valueFromValues = childArrayValues.get(i).getAsJsonObject().get("value").toString();
             String valueNew = valueFromValues.substring(1, valueFromValues.length() - 1);
 
             replaceValueMethod(childArrayTests, idFromValues, valueNew);
         }
-        //форматируем report
+        
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(jElementTests);
-        // Выводим в report.json
+    
 
         writeFile("report.json", json);
     }
@@ -56,7 +56,7 @@ public class Main {
         }
 
         for (int i = 0; i < childArray.size(); i++) {
-            // У нас рекурсия. Копаем, пока есть куда копать
+          
             if (childArray.get(i).getAsJsonObject().get("values") != null) {
                 diggerMethod(childArray, idFromValues, valueFromValues, i);
             }
@@ -88,10 +88,10 @@ public class Main {
     }
 
     public static void writeFile(String path, String writeLine) {
-        //        Записываем строку в файл
+ 
         try (
                 FileWriter writer = new FileWriter(path, false)) {
-            // запись всей строки
+        
             new FileWriter(path, false).close();
             writer.write(writeLine);
             writer.flush();
